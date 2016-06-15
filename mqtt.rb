40d72dda51e28a4b96bc16c76bda7a71ab431d2e
Alexandros <aldor94@gmail.com>
2016-06-15 21:20:53 +0300

    Add log file for hackers.txt

--- mqtt.rb
+++ mqtt.rb
@@ -7,10 +7,11 @@ require 'openssl'
   :host => 'www.techministry.gr',
   :port => 1883,
   :ssl => true,
-  :ca_file => 'path_to_your_ca'
+  :ca_file => '/var/local/mosquitto/MQTT-CA-TM.crt'
  )
 client.subscribe('techministry/spacestatus/hackers')
 client.get do |topic,message|
   # If you pass a block to the get method, then it will loop
   File.open('/usr/share/nginx/html/hackers.txt', 'w') { |file| file.truncate(0); file.write("#{message}") }
+  File.open('/usr/share/nginx/html/hackerslog.txt', 'a') { |file| file.write("#{message} #{Time.now}\n") }
 end
