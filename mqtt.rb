7972ce17f89cae562ae86745c7b9891cdf15f79d
Alexandros <aldor94@gmail.com>
2016-10-23 14:50:52 +0300

    Update mqtt subscribe url and path

--- mqtt.rb
+++ mqtt.rb
@@ -4,12 +4,12 @@ require 'openssl'
 
 # Subscribe example
  client = MQTT::Client.connect(
-  :host => 'www.techministry.rocks',
-  :port => 1883,
+  :host => 'mqtt.lambdaspace.gr',
+  :port => 8883,
   :ssl => true,
-  :ca_file => '/var/local/mosquitto/CA.crt'
+  :ca_file => '/var/local/mosquitro/cert/MQTT-LSPACE-CA.crt'
  )
-client.subscribe('techministry/spacestatus/hackers')
+client.subscribe('lambdaspace/spacestatus/hackers')
 client.get do |topic,message|
   # If you pass a block to the get method, then it will loop
   File.open('/usr/share/nginx/html/hackers.txt', 'w') { |file| file.truncate(0); file.write("#{message}") }
